8ef1b2ec5ccd780e025276de6a97c76e2b19e4c3
Fitsum Seyoum <fitsum.s@yahoo.com>
2019-05-10 21:17:26 -0400

    finished

--- Javamethod/src/com/fitsum/Main.java
+++ Javamethod/src/com/fitsum/Main.java
@@ -21,14 +21,28 @@ public class Main {
     }
 
     public static int calculateHighScore(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        } else {
+//            return 4;
+//        }
+
+
+   // we can accomplish the same result with one return statement
+   // which is more easy to read
+        int position = 4;
         if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-        } else {
-            return 4;
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
+
+        return position;
     }
 }
